2aadd94d0f0c673da6c50fbf2c9e6c83c3b07635
Jeff Evans <reznet@gmail.com>
2015-08-17 17:03:13 -0500

    Adds empty music xml writer and failing test
    Creates a new class MusicXmlWriter which takes a score and XmlWriter and will write to the xml writer.
    Adds a new test which takes an empty score, writes it, and reads it using XmlDocument and expects the movement-title element to be missing.

---
+++ MusicXml.Unit.Tests/MusicXmlWriterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MusicXml.Domain;
+using NUnit.Framework;
+using System.Xml;
+
+namespace MusicXml.Unit.Tests
+{
+    public class MusicXmlWriterTests
+    {
+        [TestFixture]
+        public class WhenScoreIsEmpty
+        {
+            private XmlDocument _document;
+
+            [TestFixtureSetUp]
+            public void SetUp()
+            {
+                var emptyScore = new Score();
+                using (XmlWriter writer = XmlWriter.Create("out.xml"))
+                {
+                    MusicXmlWriter.Write(emptyScore, writer);
+                }
+
+                _document = new XmlDocument();
+                _document.Load("out.xml");
+            }
+
+            [Test]
+            public void MovementTitleIsNotWritten()
+            {
+                Assert.That(_document.SelectSingleNode("score-partwise/movement-title"), Is.Null);
+            }
+        }
+    }
+}
---
+++ MusicXml/MusicXmlWriter.cs
@@ -0,0 +1,13 @@
+﻿using MusicXml.Domain;
+using System.Xml;
+
+namespace MusicXml
+{
+    public static class MusicXmlWriter
+    {
+        public static void Write(Score score, XmlWriter writer)
+        {
+
+        }
+    }
+}
